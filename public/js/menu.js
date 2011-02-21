ae9d2a1676d123061feb87d2497ada4fea4a53ff
Black Akula <blackakula@gmail.com>
2011-02-21 15:38:50 +0200

    Javascript finished

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,4 +5,37 @@ $(function() {
       $('.menu .item:not(.item' + $(this).attr('itemIndex') + ')').toggleClass('light' + $(this).attr('itemIndex'))
     })
   }
+
+  var roundCss = function(radius, h, w) {
+    if (typeof w == 'undefined')
+      return jQuery.extend(arguments.callee(radius, h, 'left'), arguments.callee(radius, h, 'right'))
+    var css = {}
+    css['border-' + h + '-' + w + '-radius'] = radius
+    css['-moz-border-radius-' + h + w] = radius
+    css['-webkit-border-radius-' + h + w] = radius
+    return css
+  }
+
+  var resizeMenu = function() {
+    var k = $('.menu').width() / 39
+    $('.menu').height(k * 9)
+    $('.menu a').css({fontSize: k + 'pt'})
+    
+    var item = $('.menu .item').height()
+    var itemText = .34 * item
+    var itemTab = ($('.menu').height() - itemText) / 2
+    var itemTail = item - itemText - itemTab
+    
+    $('.up .head, .down .tail').height(itemTab)
+    $('.item .text').height(itemText + 1)
+    $('.down .head, .up .tail').height(itemTail)
+
+    //border radius
+    var borderRadius = (.15 * $('.menu .item').width()) + 'px ' + (.15 * item) + 'px'
+    $('.item, .tail').css(roundCss(borderRadius, 'bottom'))
+    $('.down, .down .head').css(roundCss(borderRadius, 'top'))
+  }
+
+  resizeMenu()
+  $(window).resize(resizeMenu)
 })
