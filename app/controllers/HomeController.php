21a0996f4e5ac6c30e494c6d8fff18a5ebf39b73
Black Akula <blackakula@gmail.com>
2011-03-10 02:58:35 +0200

    Added random photo feature to index page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,7 +8,6 @@
       $data = get_config('index');
       $this->set('title', $data['title']);
       $this->set('index-menu', $data['menu']);
-      $this->set('random-photo', $data['photo']);
 
       $content = $data['content'];
       foreach ($content as $i => $item) {
@@ -24,6 +23,25 @@
         $content[$i]['text'] = str_replace('<-@->', $content[$i]['url'], $item['text']);
       }
       $this->set('content', $content);
+
+      $photo_data = get_config('photo');
+      $photo_count = 0;
+      foreach ($photo_data['photos'] as $album => $photos)
+        $photo_count += count($photos);
+      $random_index = mt_rand(1, $photo_count);
+      foreach ($photo_data['photos'] as $album => $photos) {
+        $count_photos = count($photos);
+        if ($random_index <= $count_photos) {
+          $data['photo']['url'] = get_routes()->build_path('photo') .
+                  '?a=' . urlencode($album) .
+                  '#p' . $random_index;
+          $data['photo']['img'] = $photos[$random_index - 1]['img'];
+          $data['photo']['alt'] = htmlspecialchars($photos[$random_index - 1]['text']);
+          break;
+        }
+        $random_index -= $count_photos;
+      }
+      $this->set('random-photo', $data['photo']);
     }
   }
 ?>
