92e292fedfed4051072ccf885d10d654891095a6
Black Akula <blackakula@gmail.com>
2011-03-23 02:28:00 +0200

    Base things done

---
+++ app/wordpress/functions.php
@@ -0,0 +1,54 @@
+<?php
+define('ROOT_DIR', realpath(dirname(__FILE__) . '/../../../..') . '/');
+
+class MyWPRegistry {
+  private static $data = array();
+
+  public static function set($key, $value) { self::$data[$key] = $value; }
+  public function &get($key = null) {
+    if (is_null($key))
+      return self::$data;
+    return self::$data[$key];
+  }
+}
+
+function loadConfig() {
+
+  $setup = file_get_contents(ROOT_DIR.'config'.DIRECTORY_SEPARATOR.'setup.php');
+  $setup = str_replace(array('function get_header()', '<?php', '?>'), array('function _get_header()', '', ''), $setup);
+  eval($setup);
+  
+  foreach (sfYaml::load(ROOT_DIR . 'config/base.yml') as $key => $value) {
+    $key = strtoupper($key);
+    eval("define('{$key}', '{$value}');");
+  }
+
+  MyDispatcher::config();
+
+  /* Load Routes and setting params */
+  if (!get_routes()->is_readonly())
+    require_once(get_config('CONFIG_DIR').'routes.php');
+
+  MyWPRegistry::set('Template', new ApplicationTemplate('', '', array()));
+  class MyWPController extends ApplicationController {
+    public function header() { return _get_header(); }
+  }
+  MyWPRegistry::set('Controller', new MyWPController(array()));
+
+}
+
+function _include($file) {
+  include(realpath(dirname(__FILE__)) . '/' . $file);
+}
+
+function _js($src, $global = false) {
+  return MyWPRegistry::get('Template')->_js($src, $global);
+}
+
+function _img($src, $alt = null, $global = false, $attr = array()) {
+  return MyWPRegistry::get('Template')->_img($src, $alt, $attr);
+}
+
+function _get($key = null) {
+  return MyWPRegistry::get('Controller')->get($key);
+}
---
+++ app/wordpress/make.php
@@ -0,0 +1,62 @@
+<?php
+  
+  $domain = 'http://al-mix.org.ua/';
+  $cur_dir = realpath(dirname(__FILE__));
+  $lib_dir = realpath($cur_dir . '/../../lib/my') . '/';
+  include_once($lib_dir . 'Haml.php');
+
+  function make_folder($in_dir, $out_dir, $skipMake = false) {
+    $d = opendir($in_dir);
+    while ($f = readdir($d)) {
+      $isHaml = preg_match('/\.haml$/', $f) != 0;
+      $isPhp = preg_match('/\.php$/', $f) != 0;
+      if (!$isHaml && !$isPhp) {
+        continue;
+      }
+      if ($skipMake && $isPhp && ($f == 'make.php')) {
+        continue;
+      }
+
+      $php = file_get_contents($in_dir . '/' . $f);
+      if ($isHaml) {
+        $php = Haml::parse2($php);
+      }
+      file_put_contents($out_dir . preg_replace('/\.haml$/', '.php', $f), $php);
+    }
+  }
+
+  function clear_folder($dir) {
+    $d = opendir($dir);
+    while ($f = readdir($d)) {
+      if ($f == '.' || $f == '..') {
+        continue;
+      }
+
+      unlink($dir . '/' . $f);
+    }
+  }
+
+  foreach (array(
+      'mix' => array('alamix', 'AlaMix', $domain . 'mix/')
+  ) as $folder => $theme) {
+
+    $in_dir = realpath($cur_dir . '/' . $folder);
+    $out_dir = realpath($cur_dir . '/../../' . $folder . '/wp-content/themes/' . $theme[0]) . '/';
+    
+    clear_folder($out_dir . '.');
+    
+    $style = <<<STYLE
+/*
+Theme Name: {$theme[1]}
+Theme URI: {$theme[2]}
+Description: This is the special theme "{$theme[1]}" designed for $domain
+Author: Akulinin Sergey, Alina Mikhailova
+Author URI: $domain
+*/
+STYLE;
+    $style .= "\n" . file_get_contents($in_dir . '/style.css');
+
+    file_put_contents($out_dir . 'style.css', $style);
+    make_folder($in_dir, $out_dir);
+    make_folder($cur_dir, $out_dir, true);
+  }
