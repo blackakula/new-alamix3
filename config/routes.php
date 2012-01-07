8cac0f33e5e465e10fe2049cf3d9756fc21b766a
Black Akula <blackakula@gmail.com>
2012-01-07 23:16:30 +0200

    Favicon is added

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
   $r = get_routes();
   
   $r->named('root', '/', array('controller' => 'home'));
-  
+
   $r->named('photo', '/photo', array('controller' => 'photo'));
   $r->connect('/ajax/photo.json', array('controller' => 'photo', 'action' => 'thumbs'));
 
--- lib/my/MyDispatcher.php
+++ lib/my/MyDispatcher.php
@@ -9,6 +9,7 @@
       self::$contoller->set('show_menu_and_logo', false);
       self::$contoller->set('stylesheets', array('404.css'));
       self::$contoller->set('javascripts', array());
+      self::$contoller->set('title', 'помилка 404');
     }
   }
 ?>
