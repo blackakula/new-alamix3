54f6e14d71cea90f91b353962318b7f8a893ce5f
Black Akula <blackakula@gmail.com>
2012-01-09 21:24:52 +0200

    Content is started

--- app/controllers/LinksController.php
+++ app/controllers/LinksController.php
@@ -1,5 +1,6 @@
 <?php
   class LinksController extends ApplicationController {
+    const DELIMITER = '<***>';
     private $data;
 
     public function  __construct($params) {
@@ -16,6 +17,38 @@
       $this->set('menu-replace', 3);
 
       $this->set('title', $this->data['title']);
+
+      $columns = array();
+      $columnIndex = 0;
+      
+      foreach ($this->data['content'] as $item) {
+        $this->_newColumn($columnIndex, $columns);
+
+        $column_item = &$columns[$columnIndex];
+        if (count($column_item) > 0) $column_item[] = array('space');
+        $column_item[] = array('colors', $item['colors']);
+        $link = !isset($item['link']) || empty($item['link']) ? null : $item['link'];
+        $column_item[] = array('title', array($item['title'], $link));
+
+        foreach (explode(self::DELIMITER, $item['text']) as $part) {
+          $part = trim($part);
+          if (!empty($part)) {
+            $this->_newColumn($columnIndex, $columns);
+            if ($link) {
+              $part = str_replace('<-@->', $link, $part);
+            }
+            $columns[$columnIndex][] = array('text', $part);
+          }
+          ++$columnIndex;
+        }
+        --$columnIndex;
+      }
+      $this->set('columns', $columns);
+    }
+  
+    private function _newColumn($column_index, &$columns) {
+     if (!array_key_exists($column_index, $columns))
+     $columns[$column_index] = array();
     }
   }
 ?>
