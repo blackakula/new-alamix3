93b116e622432a1e30e1c55f6c121262a241ab31
Black Akula <blackakula@gmail.com>
2012-02-11 13:35:45 +0200

    CSS styles are done for portfolio

--- app/controllers/SimpleController.php
+++ app/controllers/SimpleController.php
@@ -1,11 +1,21 @@
 <?php
   class SimpleController extends ApplicationController {
+    private $data;
+
+    protected function _prepareData($configName) {
+      $this->data = get_config($configName);
+      $this->set('title', $this->data['title']);
+    }
+
     public function heart() {
+      $this->_prepareData('heart');
       $this->set('hide_menu', 'heart');
       $this->set('stylesheets', array('heart.css'));
     }
 
     public function portfolio() {
+      $this->_prepareData('portfolio');
       $this->set('stylesheets', array('simple.css'));
+      $this->set('content', $this->data['content']);
     }
   }
