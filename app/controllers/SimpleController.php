ba6a787a0e85e0f44a216a70f7afce6ca4dadec9
Black Akula <blackakula@gmail.com>
2012-07-07 00:36:40 +0300

    Implemented issuu portfolio

--- app/controllers/SimpleController.php
+++ app/controllers/SimpleController.php
@@ -17,6 +17,16 @@
       $this->_prepareData('portfolio');
       $this->set('stylesheets', array('simple.css'));
       $this->push('javascripts', 'portfolio.js');
+      foreach ($this->data['content'] as &$row) {
+          foreach ($row['items'] as &$item) {
+              if (is_array($item) && isset($item['document_id'])) {
+                  if (!isset($item['color'])) {
+                      $item['color'] = '000000';
+                  }
+                  $item = '<div style="background-color:#' . $item['color'] . '"><object style="width:420px;height:266px" ><param name="movie" value="http://static.issuu.com/webembed/viewers/style1/v2/IssuuReader.swf?mode=mini&amp;embedBackground=%23' . $item['color'] . '&amp;backgroundColor=%23222222&amp;documentId=' . $item['document_id'] . '" /><param name="allowfullscreen" value="true"/><param name="menu" value="false"/><param name="wmode" value="transparent"/><embed src="http://static.issuu.com/webembed/viewers/style1/v2/IssuuReader.swf" type="application/x-shockwave-flash" allowfullscreen="true" menu="false" wmode="transparent" style="width:420px;height:266px" flashvars="mode=mini&amp;embedBackground=%23' . $item['color'] . '&amp;backgroundColor=%23222222&amp;documentId=' . $item['document_id'] . '" /></object></div>';
+              }
+          }
+      }
       $this->set('content', $this->data['content']);
     }
   }
